2d62e9668770b0c8b85ef6ae4a8189b09d56d036
alextavella <alex.tavella@azos.com.br>
2025-07-10 12:14:14 -0300

    feat: get rooms with pagination

--- apps/backend/src/http/routes/get-rooms.ts
+++ apps/backend/src/http/routes/get-rooms.ts
@@ -1,10 +1,26 @@
 import { count, desc, eq } from 'drizzle-orm'
-import type { FastifyInstance } from 'fastify'
+import type { FastifyInstance, RouteShorthandOptions } from 'fastify'
+import { z } from 'zod'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
 
+const querySchema = z.object({
+  page: z.coerce.number().optional().default(1),
+  limit: z.coerce.number().optional().default(10)
+})
+
+type QuerySchema = z.infer<typeof querySchema>
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    querystring: querySchema
+  }
+}
+
 export const getRoomsRoute = (app: FastifyInstance) => {
-  app.get('/rooms', async (_, reply) => {
+  app.get('/rooms', opts, async (request, reply) => {
+    const { page, limit } = request.query as QuerySchema
+
     const result = await db
       .select({
         id: schema.rooms.id,
@@ -16,6 +32,8 @@ export const getRoomsRoute = (app: FastifyInstance) => {
       .leftJoin(schema.questions, eq(schema.rooms.id, schema.questions.roomId))
       .groupBy(schema.rooms.id)
       .orderBy(desc(schema.rooms.createdAt))
+      .limit(limit)
+      .offset((page - 1) * limit)
 
     return reply.status(200).send({ rooms: result })
   })

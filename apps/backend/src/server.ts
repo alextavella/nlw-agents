3e1fed5aa8e721469b0f328449568f54d17ccab1
alextavella <alex.tavella@azos.com.br>
2025-07-10 11:48:15 -0300

    feat: rooms and questions

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -2,20 +2,25 @@ import { fastifyCors } from '@fastify/cors'
 import { fastify } from 'fastify'
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { createRoomRoute } from './http/routes/create-room.ts'
+import { getRoomQuestionsRoute } from './http/routes/get-room-questions.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
-const app = fastify({ logger: true }).withTypeProvider<ZodTypeProvider>()
+const app = fastify({ logger: false }).withTypeProvider<ZodTypeProvider>()
 
-app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 app.register(fastifyCors, { origin: '*' })
-app.get('/', () => {
-  return 'Hello World!'
+app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestionsRoute)
+
+app.get('/health', () => {
+  return "I'm healthy!"
 })
 
-app.register(getRoomsRoute)
-
 app.listen({ port: env.PORT }).then(() => {
-  app.log.info(`🚀 Server is running at http://localhost:${env.PORT}`)
+  // biome-ignore lint/suspicious/noConsole: only log
+  console.log(`🚀 Server is running at http://localhost:${env.PORT}`)
 })

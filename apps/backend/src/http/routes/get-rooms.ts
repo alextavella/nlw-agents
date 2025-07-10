732241c5ff787467e513f4581cd71bf1eafe1b66
alextavella <alex.tavella@azos.com.br>
2025-07-10 11:56:24 -0300

    feat: return questions count

--- apps/backend/src/http/routes/get-rooms.ts
+++ apps/backend/src/http/routes/get-rooms.ts
@@ -1,17 +1,22 @@
-import { desc } from 'drizzle-orm'
+import { count, desc, eq } from 'drizzle-orm'
 import type { FastifyInstance } from 'fastify'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
 
 export const getRoomsRoute = (app: FastifyInstance) => {
   app.get('/rooms', async (_, reply) => {
-    const rooms = await db
+    const result = await db
       .select({
         id: schema.rooms.id,
-        name: schema.rooms.name
+        name: schema.rooms.name,
+        questionsCount: count(schema.questions.id),
+        createdAt: schema.rooms.createdAt
       })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.rooms.id, schema.questions.roomId))
+      .groupBy(schema.rooms.id)
       .orderBy(desc(schema.rooms.createdAt))
-    return reply.status(200).send({ rooms })
+
+    return reply.status(200).send({ rooms: result })
   })
 }
